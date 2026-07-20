7f4706df93355bfcf75ac76e78b25f5605acfa0a
HARIS-MITHA <147986826+HARIS-MITHA@users.noreply.github.com>
2026-07-20 13:36:37 +0530

    Time: 0 ms (100%), Space: 12.4 MB (53.01%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        write=0
+        for i in range(len(nums)):
+            if nums[i]!=val:
+                nums[write]=nums[i]
+                write+=1
+        return write
+        
